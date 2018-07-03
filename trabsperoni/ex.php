1d1d4b10fc36549d44957f812490707e0628e7bd
FelipexConquerOfWorlds <felipepborba@hotmail.com>
2018-07-03 16:57:59 -0300

    Add files via upload

--- trabsperoni/ex.php
+++ trabsperoni/ex.php
@@ -3,13 +3,21 @@
     $pOc = $_GET['ip1'];
     $sOc = $_GET['ip2'];
     $tOc = $_GET['ip3'];
-    $mascaraEmbits = $_GET['ip4'];
+    $qOc = $_GET['ip4'];
+    $mascaraEmbits = $_GET['ip5'];
 
     $numBits = 32 - $mascaraEmbits;
-
-    function numConexoes($numeroDeBits)
+/*
+   $pOc = 112;
+   $sOc = 168;
+   $tOc = 112;
+   $qOc = 32;
+   $mascaraEmbits = 27;
+    $numBits = 32 - $mascaraEmbits;
+*/
+    function numConexoes($numBits)
     {
-        $numBits = 32 - $numeroDeBits;
+
         $numeroDeConexoes = pow(2, $numBits);
         return $numeroDeConexoes;
     }
@@ -24,24 +32,24 @@
         return $resultado;
     }
 
-    function primeiroNumero($tOc){
-        $resultado = $tOc + 1;
+    function primeiroNumero($qOc){
+        $resultado = $qOc + 1;
         return $resultado;
     }
 
-    function ultimoNumerodHost($tOc, $numeroDeConexoes){
-        $resultado = $tOc + $numeroDeConexoes - 2;
+    function ultimoNumerodHost($qOc, $numeroDeConexoes){
+        $resultado = $qOc + $numeroDeConexoes - 2;
         return $resultado;
     }
 
-    function ultimoNumero($tOc, $numeroDeConexoes){
-        $resultado = $tOc + $numeroDeConexoes - 1;
+    function ultimoNumero($qOc, $numeroDeConexoes){
+        $resultado = $qOc + $numeroDeConexoes - 1;
         return $resultado;
     }
 
     function mascaraDecimal($numeroDeConexoes){
         $mascara = 256 - $numeroDeConexoes;
-        $resultado = "255.255.255" + $mascara;
+        $resultado = "255.255.255.".$mascara;
         return $resultado;
     }
 
@@ -59,11 +67,18 @@
         }
     }
 
-    function publicoOuPrivado($pOc, $sOc){
-        if ($pOc = 10 || ($pOc = 172 && $sOc > 15 && $sOc < 32) || ($pOc = 192 && $sOc = 168)){
+    function publicoOuPrivado($pOc, $sOc)
+    {
+        if ($pOc = 10 || ($pOc = 172 && $sOc > 15 && $sOc < 32) || ($pOc = 192 && $sOc = 168)) {
             return "privado";
-        } else{
+        } else {
             return "publico";
         }
     }
+    
+    $leiDoRetorno = array(numConexoes($numBits), qtdHost(numConexoes($numBits)), qntSubRedes(numConexoes($numBits)), primeiroNumero($tOc), ultimoNumerodHost($tOc, numConexoes($numBits)), ultimoNumero($tOc, numConexoes($numBits)), mascaraDecimal(numConexoes($numBits)), classePertence($pOc), publicoOuPrivado($pOc,$sOc));
+    foreach ($leiDoRetorno as $choraBoy){
+        echo "$choraBoy \n";
+    }
+    
     ?>
